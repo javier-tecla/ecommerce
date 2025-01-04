4de107a26cd1f93743d0ec54c48b4e28521f9c3b
Javier Borjas <cristman11@gmail.com>
2025-01-04 00:30:46 -0300

    Formatear datos en objeto para las fuentes, fontFamily, fontBody, fontSlide

--- web/views/template.php
+++ web/views/template.php
@@ -14,17 +14,15 @@ $url = "templates?linkTo=active_template&equalTo=ok";
 $method = "GET";
 $fields = array();
 
-$template = CurlController::request($url,$method,$fields);
+$template = CurlController::request($url, $method, $fields);
 
 
-if($template->status == 200){
+if ($template->status == 200) {
 
     $template = $template->results[0];
-    
+} else {
 
-}else{
-
-//Redireccionar a página 500
+    //Redireccionar a página 500
 
 }
 
@@ -36,10 +34,18 @@ $keywords = null;
 
 foreach (json_decode($template->keywords_template, true) as $key => $value) {
 
-    $keywords .= $value.", ";
+    $keywords .= $value . ", ";
 }
 
 $keywords = substr($keywords, 0, -2);
+
+/*=================================================
+Datos en Objeto
+=================================================*/
+
+$fontFamily = json_decode($template->fonts_template)->fontFamily;
+$fontBody = json_decode($template->fonts_template)->fontBody;
+$fontSlide = json_decode($template->fonts_template)->fontSlide;
 
 
 
@@ -57,15 +63,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $template->title_template ?></title>
 
-    <meta name="description content="<?php echo $template->description_template ?>">
+    <meta name="description content=" <?php echo $template->description_template ?>">
     <meta name="keywords" content="<?php echo $keywords ?>">
 
     <link rel="icon" href="<?php echo $path ?>views/assets/img/template/<?php echo $template->id_template ?>/<?php echo $template->icon_template ?>">
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <?php echo urldecode($fontFamily) ?>
 
 
     <!-- CSS -->
@@ -85,22 +89,34 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link rel="stylesheet" href="<?php echo $path ?>views/assets/css/template/template.css">
 
     <link rel="stylesheet" href="<?php echo $path ?>views/assets/css/products/products.css">
-    
+
+    <style>
+        body {
+            font-family: '<?php echo $fontBody ?>', serif;
+
+        }
+
+        .slideOpt h1,  .slideOpt h2, .slideOpt h3{
+            font-family: '<?php echo $fontSlide ?>', sans-serif;
+        }
+
+    </style>
+
 
     <!-- JS -->
 
-    
+
     <!-- jQuery -->
     <script src="<?php echo $path ?>views/assets/js/plugins/jquery/jquery.min.js"></script>
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    
+
     <!-- JDSlider 
     https://www.jqueryscript.net/slider/Carousel-Slideshow-jdSlider.html -->
     <script src="<?php echo $path ?>views/assets/js/plugins/jdSlider/jdSlider.js""></script>
 
     <!-- Knob -->
-    <script src="<?php echo $path ?>views/assets/js/plugins/knob/knob.js""></script>
+    <script src=" <?php echo $path ?>views/assets/js/plugins/knob/knob.js""></script>
 </head>
 
 <body class="hold-transition sidebar-collapse layout-top-nav">
