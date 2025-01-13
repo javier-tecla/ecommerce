162f32298f9ec00ec1e4dd6bb233cbb98d6505fe
Javier Borjas <cristman11@gmail.com>
2025-01-13 00:33:56 -0300

    Paginas del panel administrativo

--- web/views/pages/admin/admin.php
+++ web/views/pages/admin/admin.php
@@ -7,7 +7,30 @@ if (!isset($_SESSION["admin"])) {
     include "login/login.php";
 } else {
 
-    include "tablero/tablero.php";
+    if (!empty($routesArray[1])) {
+
+        if (
+            $routesArray[1] == "administradores" ||
+            $routesArray[1] == "plantillas" ||
+            $routesArray[1] == "integraciones" ||
+            $routesArray[1] == "slides" ||
+            $routesArray[1] == "banners" ||
+            $routesArray[1] == "categorias" ||
+            $routesArray[1] == "subcategorias" ||
+            $routesArray[1] == "inventario" ||
+            $routesArray[1] == "mensajes" ||
+            $routesArray[1] == "pedidos" ||
+            $routesArray[1] == "disputas" ||
+            $routesArray[1] == "informes" ||
+            $routesArray[1] == "clientes"
+            ) {
+
+            include $routesArray[1] . "/" . $routesArray[1] . ".php";
+        }
+    } else {
+
+        include "tablero/tablero.php";
+    }
 }
 
 ?>
