5a2b54a95f5c1ae815f8aa183b8730ae79c1b33c
Javier Borjas <cristman11@gmail.com>
2025-01-13 22:02:46 -0300

    Marcar el vinculo activo

--- web/views/modules/sidebar.php
+++ web/views/modules/sidebar.php
@@ -23,8 +23,8 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
           <li class="nav-item">
-            <a href="/admin/administradores" class="nav-link">
-              <i class="fas fa-user-cog"></i>
+            <a href="/admin/administradores" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "administradores"): ?> active <?php endif ?>">
+              <i class="nav-icon fas fa-user-cog"></i>
               <p>
                 Administradores
               </p>
@@ -43,13 +43,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="/admin/plantillas" class="nav-link">
+                <a href="/admin/plantillas" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "plantillas"): ?> active <?php endif ?>">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Plantillas</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/admin/integraciones" class="nav-link">
+                <a href="/admin/integraciones" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "integraciones"): ?> active <?php endif ?>">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Integraciones</p>
                 </a>
@@ -67,13 +67,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="/admin/slides" class="nav-link">
+                <a href="/admin/slides" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "slides"): ?> active <?php endif ?>">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Slides</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/admin/banners" class="nav-link">
+                <a href="/admin/banners" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "banners"): ?> active <?php endif ?>">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Banners</p>
                 </a>
@@ -87,32 +87,33 @@
               <p>
                 Productos
                 <i class="right fas fa-angle-left"></i>
+                <span class="right badge badge-primary mr-1">5</span>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="/admin/categorias" class="nav-link">
+                <a href="/admin/categorias" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "categorias"): ?> active <?php endif ?>">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Categorías</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/admin/subcategorias" class="nav-link">
+                <a href="/admin/subcategorias" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "subcategorias"): ?> active <?php endif ?>"">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Subcategorías</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/admin/inventario" class="nav-link">
+                <a href="/admin/inventario" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "inventario"): ?> active <?php endif ?>">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Inventario</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/admin/mensajes" class="nav-link">
+                <a href="/admin/mensajes" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "mensajes"): ?> active <?php endif ?>">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Mensajes</p>
-                  <span class="right badge badge-danger">5</span>
+                  <span class="right badge badge-info">5</span>
                 </a>
               </li>
             </ul>
@@ -124,25 +125,26 @@
               <p>
                 Ventas
                 <i class="right fas fa-angle-left"></i>
+                <span class="right badge badge-warning mr-1">5</span>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="/admin/pedidos" class="nav-link">
+                <a href="/admin/pedidos" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "pedidos"): ?> active <?php endif ?>">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Pedidos</p>
-                  <span class="right badge badge-danger">1</span>
+                  <span class="right badge badge-success">2</span>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/admin/disputas" class="nav-link">
+                <a href="/admin/disputas" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "disputas"): ?> active <?php endif ?>">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>disputas</p>
+                  <p>Disputas</p>
                   <span class="right badge badge-danger">3</span>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="/admin/informes" class="nav-link">
+                <a href="/admin/informes" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "informes"): ?> active <?php endif ?>">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Informes</p>
                 </a>
@@ -151,7 +153,7 @@
           </li>
 
           <li class="nav-item">
-            <a href="/admin/clientes" class="nav-link">
+            <a href="/admin/clientes" class="nav-link <?php if (!empty($routesArray[1]) && $routesArray[1] == "clientes"): ?> active <?php endif ?>">
               <i class="nav-icon fas fa-users"></i>
               <p>
                 Clientes
@@ -161,7 +163,7 @@
 
           <li class="nav-item">
             <a href="/salir" class="nav-link">
-              <i class="fas fa-sign-out-alt"></i>
+              <i class="nav-icon fas fa-sign-out-alt"></i>
               <p>
                 Salir
               </p>
