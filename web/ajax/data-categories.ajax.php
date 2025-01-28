f4bf77d3ddf9c224550e16adfd1b046fac558d79
Javier Borjas <cristman11@gmail.com>
2025-01-28 02:44:21 -0300

    Mejorar la apariencia de la tabla categorias

--- web/ajax/data-categories.ajax.php
+++ web/ajax/data-categories.ajax.php
@@ -3,40 +3,37 @@
 require_once "../controllers/curl.controller.php";
 require_once "../controllers/template.controller.php";
 
-class DataTableController
-{
+class DatatableController{
 
-    public function data()
-    {
+	public function data(){
 
-        if (!empty($_POST)) {
+		if(!empty($_POST)){
 
-            /*===============================================
+			/*=============================================
             Capturando y organizando las variables POST de DT
-            =================================================*/
+            =============================================*/
 
-            $draw = $_POST["draw"]; //Contador utilizado por DataTables para garantizar que los retornos de Ajax de las solicitudes de procesamiento del lado del servidor sean dibujados en secuencia por DataTables
-            //echo '<pre>'; print_r($draw); echo '</pre>';
+			$draw = $_POST["draw"]; //Contador utilizado por DataTables para garantizar que los retornos de Ajax de las solicitudes de procesamiento del lado del servidor sean dibujados en secuencia por DataTables 
+			// echo '<pre>$draw '; print_r($draw); echo '</pre>';
 
-            $orderByColumnIndex = $_POST["order"][0]["column"]; //Índice de la columna de clasificación (0 basado en el índice, es decir, 0 es el primer registro)
-            //echo '<pre>'; print_r($orderByColumnIndex); echo '</pre>';
+			$orderByColumnIndex = $_POST["order"][0]["column"]; //Índice de la columna de clasificación (0 basado en el índice, es decir, 0 es el primer registro)
+			// echo '<pre>$orderByColumnIndex '; print_r($orderByColumnIndex); echo '</pre>';
 
-            $orderBy = $_POST["columns"][$orderByColumnIndex]["data"]; //Obtener el nombre de la columna de clasificación de su índice
-            //echo '<pre>'; print_r($orderBy); echo '</pre>';
+			$orderBy = $_POST["columns"][$orderByColumnIndex]["data"];//Obtener el nombre de la columna de clasificación de su índice
+			// echo '<pre>$orderBy '; print_r($orderBy); echo '</pre>';
 
-            $orderType = $_POST["order"][0]["dir"]; // Obtener el orden ASC o DESC
-            //echo '<pre>'; print_r($orderType); echo '</pre>';
+			$orderType = $_POST["order"][0]["dir"]; // Obtener el orden ASC o DESC
+			// echo '<pre>$orderType '; print_r($orderType); echo '</pre>';
 
-            $start = $_POST["start"]; //Indicador de primer registro de paginación.
-            //echo '<pre>'; print_r($start); echo '</pre>';
+			$start = $_POST["start"];//Indicador de primer registro de paginación.
+			// echo '<pre>$start '; print_r($start); echo '</pre>';
 
-            $length = $_POST["length"]; //Indicador de la longitud de la paginación.
-            //echo '<pre>'; print_r($length); echo '</pre>';
+			$length = $_POST["length"];//Indicador de la longitud de la paginación.
+			// echo '<pre>$length '; print_r($length); echo '</pre>';
 
-
-            /*=================================================
-            El total de registro de la data
-            =================================================*/
+			/*=============================================
+            El total de registros de la data
+            =============================================*/
 
             $url = "categories?select=id_category";
             $method = "GET";
@@ -44,159 +41,192 @@ class DataTableController
 
             $response = CurlController::request($url, $method, $fields);
 
-            if ($response->status == 200) {
+            if($response->status == 200){
 
-                $totalData = $response->total;
-            } else {
+            	$totalData = $response->total;
+            
+            }else{
 
-                echo '{
-                    "Draw": 1,
-                    "recordsTotal": 0,
-                    "recordsFiltered": 0,
-                    "data":[]}';
+            	echo '{
+            		"Draw": 1,
+					"recordsTotal": 0,
+				    "recordsFiltered": 0,
+				    "data":[]}';
 
-                return;
+            	return;
+
             }
 
-            $select = "*";
-
+            $select = "id_category,status_category,name_category,url_category,image_category,description_category,keywords_category,subcategories_category,products_category,views_category,date_updated_category";
 
             /*=============================================
            	Búsqueda de datos
-            =============================================*/
+            =============================================*/	
 
-            if (!empty($_POST['search']['value'])) {
+            if(!empty($_POST['search']['value'])){	
 
-                if (preg_match('/^[0-9A-Za-zñÑáéíóú ]{1,}$/', $_POST['search']['value'])) {
+            	if(preg_match('/^[0-9A-Za-zñÑáéíóú ]{1,}$/',$_POST['search']['value'])){
 
-                    $linkTo = ["name_category", "url_category", "description_category", "keywords_category", "date_updated_category"];
+            		$linkTo = ["name_category", "url_category","description_category","keywords_category","date_updated_category"];
 
-                    $search = str_replace(" ", "_", $_POST['search']['value']);
+            		$search = str_replace(" ","_",$_POST['search']['value']);
 
-                    foreach ($linkTo as $key => $value) {
+            		foreach ($linkTo as $key => $value) {
+            			
+            			$url = "categories?select=".$select."&linkTo=".$value."&search=".$search."&orderBy=".$orderBy."&orderMode=".$orderType."&startAt=".$start."&endAt=".$length;
 
-                        $url = "categories?select=" . $select . "&linkTo=" . $value . "&search=" . $search . "&orderBy=" . $orderBy . "&orderMode=" . $orderType . "&startAt=" . $start . "&endAt=" . $length;
+	            		$data = CurlController::request($url, $method, $fields)->results;
 
-                        $data = CurlController::request($url, $method, $fields)->results;
+	            		if($data == "Not Found"){
 
-                        if ($data == "Not Found") {
+	            			$data = array();
+	            			$recordsFiltered = 0;
+	            		
+	            		}else{
 
-                            $data = array();
-                            $recordsFiltered = 0;
-                        } else {
+	            			$recordsFiltered = count($data);
+	            			break;
+	            		}
+            		}
 
-                            $recordsFiltered = count($data);
-                            break;
-                        }
-                    }
-                } else {
+            	}else{
 
-                    echo '{
-                        "Draw": 1,
-                        "recordsTotal": 0,
-                        "recordsFiltered": 0,
-                        "data":[]}';
-    
-                    return;
-                }
-            } else {
+            		echo '{
+            		"Draw": 1,
+					"recordsTotal": 0,
+				    "recordsFiltered": 0,
+				    "data":[]}';
 
-                /*=============================================
+                	return;
+
+            	}
+
+            }else{
+
+	            /*=============================================
 	            Seleccionar datos
 	            =============================================*/
 
-                $url = "categories?select=" . $select . "&orderBy=" . $orderBy . "&orderMode=" . $orderType . "&startAt=" . $start . "&endAt=" . $length;
-                $data = CurlController::request($url, $method, $fields)->results;
+	            $url = "categories?select=".$select."&orderBy=".$orderBy."&orderMode=".$orderType."&startAt=".$start."&endAt=".$length;
+	            $data = CurlController::request($url, $method, $fields)->results;
 
-                $recordsFiltered = $totalData;
-            }
+	            $recordsFiltered = $totalData;
 
-            /*=================================================
+	        }
+           
+            /*=============================================
             Cuando la data viene vacía
-            =================================================*/
+            =============================================*/
 
-            if (empty($data)) {
+             if(empty($data)){
 
-                echo '{
-                    "Draw": 1,
-                    "recordsTotal": 0,
-                    "recordsFiltered": 0,
-                    "data":[]}';
+            	echo '{
+            		"Draw": 1,
+					"recordsTotal": 0,
+				    "recordsFiltered": 0,
+				    "data":[]}';
 
-                return;
+            	return;
+
             }
 
-            /*=================================================
+            /*=============================================
             Construimos el dato JSON a regresar
-            =================================================*/
+            =============================================*/
 
             $dataJson = '{
-                "Draw": ' . intval($draw) . ',
-                "recordsTotal": ' . $totalData . ',
-                "recordsFiltered": ' . $recordsFiltered . ',
-                "data": [';
+				"Draw": '.intval($draw).',
+				"recordsTotal": '.$totalData.',
+				"recordsFiltered": '.$recordsFiltered.',
+				"data": [';
 
-            foreach ($data as $key => $value) {
+				foreach ($data as $key => $value) {
 
-                  /*=================================================
-                    SATATUS
-                   =================================================*/
+					/*=============================================
+            		STATUS
+            		=============================================*/
 
-                   $status_category = $value->status_category;
+            		if($value->status_category == 1){
 
-                   /*=================================================
-                    TEXTOS
-                   =================================================*/
+	            		$status_category = "<input type='checkbox' data-size='mini' data-off-color='danger' data-on-color='dark' checked='true' idItem='".base64_encode($value->id_category)."' table='categories' column='category'>";
 
-                $name_category = $value->name_category;
-                $url_category = $value->url_category;
-                $image_category = $value->image_category;
-                $description_category = $value->description_category;
-                $keywords_category = $value->keywords_category;
-                $subcategories_category = $value->subcategories_category;
-                $products_category = $value->products_category;
-                $views_category = $value->views_category;
-                $date_updated_category = $value->date_updated_category;
+	            	}else{
 
-                $actions = "<div class='btn-group'>
-									<a href='/admin/categorias/gestion?admin=" . base64_encode($value->id_category) . "' class='btn bg-purple border-0 rounded-pill mr-2 btn-sm px-3'>
+	            		$status_category = "<input type='checkbox' data-size='mini' data-bootstrap-switch data-off-color='danger' data-on-color='dark' idItem='".base64_encode($value->id_category)."' table='categories' column='category'>";
+	            	}
+
+            		/*=============================================
+            		TEXTOS
+            		=============================================*/
+
+					$name_category = $value->name_category;
+
+
+					$url_category = "<a href='/".$value->url_category."' target='_blank' class='badge badge-light px-3 py-1 border rounded-pill'>/".$value->url_category."</a>";
+
+					$image_category =  "<img src='/views/assets/img/categories/".$value->url_category."/".$value->image_category."' class='img-thumbnail rounded'>";
+
+					$description_category = templateController::reduceText($value->description_category, 25);
+
+					$keywords_category = "";
+
+					$keywordsArray = explode(",",$value->keywords_category);
+
+					foreach ($keywordsArray as $index => $item) {
+						
+						$keywords_category .= "<span class='badge badge-primary rounded-pill px-3 py-1'>".$item."</span>";
+					}
+
+					$subcategories_category = $value->subcategories_category;
+
+					$products_category = $value->products_category;
+
+					$views_category = "<span class='badge badge-warning rounded-pill px-3 py-1'><i class='fas fa-eye'></i> ".$value->views_category."</span>";
+
+            		$date_updated_category = $value->date_updated_category;
+					
+            		$actions = "<div class='btn-group'>
+									<a href='/admin/categorias/gestion?category=".base64_encode($value->id_category)."' class='btn bg-purple border-0 rounded-pill mr-2 btn-sm px-3'>
 										<i class='fas fa-pencil-alt text-white'></i>
 									</a>
-									<button class='btn btn-dark border-0 rounded-pill mr-2 btn-sm px-3 deleteItem' rol='admin' table='categories' column='category' idItem='" . base64_encode($value->id_category) . "'>
+									<button class='btn btn-dark border-0 rounded-pill mr-2 btn-sm px-3 deleteItem' rol='admin' table='categories' column='category' idItem='".base64_encode($value->id_category)."'>
 										<i class='fas fa-trash-alt text-white'></i>
 									</button>
 								</div>";
 
-                $actions = TemplateController::htmlClean($actions);
+					$actions = TemplateController::htmlClean($actions);
 
-                $dataJson .= '{ 
-                        "id_category":"' . ($start + $key + 1) . '",
-                        "status_category":"' . $status_category . '",
-                        "name_category":"' . $name_category . '",
-                        "url_category":"' . $url_category . '",
-                        "image_category":"' . $image_category . '",  
-                        "description_category":"' . $description_category . '",  
-                        "keywords_category":"' . $keywords_category . '",  
-                        "subcategories_category":"' . $subcategories_category . '",  
-                        "products_category":"' . $products_category . '",  
-                        "views_category":"' . $views_category . '",  
-                        "date_updated_category":"' . $date_updated_category . '" ,
-                        "actions":"' . $actions . '"             
-                    },';
-            }
+					$dataJson.='{ 
+						"id_category":"'.($start+$key+1).'",
+						"status_category":"'.$status_category.'",
+						"name_category":"'.$name_category.'",
+						"url_category":"'.$url_category.'",
+						"image_category":"'.$image_category.'",
+						"description_category":"'.$description_category.'",
+						"keywords_category":"'.$keywords_category.'",
+						"subcategories_category":"'.$subcategories_category.'",
+						"products_category":"'.$products_category.'",
+						"views_category":"'.$views_category.'",
+						"date_updated_category":"'.$date_updated_category.'",
+						"actions":"'.$actions.'"
+					},';
+				}
 
-            $dataJson = substr($dataJson, 0, -1); // este substr quita el último caracter de la cadena, que es una coma, para impedir que rompa la tabla
+			$dataJson = substr($dataJson,0,-1); // este substr quita el último caracter de la cadena, que es una coma, para impedir que rompa la tabla
 
-            $dataJson  .= ']}';
+			$dataJson .= ']}';
 
-            echo $dataJson;
-        }
-    }
+			echo $dataJson;
+
+		}
+
+	}
+
 }
 
-/*=================================================
- Activar función DataTable
- =================================================*/
+/*=============================================
+Activar función DataTable
+=============================================*/ 
 
-$data = new DataTableController();
-$data->data();
+$data = new DatatableController();
+$data -> data();
