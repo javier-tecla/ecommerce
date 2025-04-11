803ded6aeec38cec867e22fb788fd9a652118718
Javier Borjas <cristman11@gmail.com>
2025-04-10 22:19:46 -0300

    Gestor Plantilla

---
+++ web/views/pages/admin/plantillas/modules/listado.php
@@ -0,0 +1,71 @@
+<div class="content pb-5">
+
+    <div class="container">
+
+        <div class="carrd">
+
+            <h3 class="card-header">
+                <a href="/admin/plantillas/gestion" class="btn bg-default templateColor py-2 px-3 btn-sm rounded-pill">Agregar Plantilla</a>
+            </h3>
+
+        </div>
+
+        <div class="card-body">
+
+            <table id="tables" class="table table-bordered table-striped templatesTable">
+
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Estado</th>
+                        <th>Logo</th>
+                        <th>Icono</th>
+                        <th>Imagen</th>
+                        <th>Título</th>
+                        <th>Descripción</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    <tr>
+                        <td>1</td>
+                        <td>
+                            <div class="form-check">
+                                <input type="radio" class="form-check-input" id="radio1" name="optradio" value="option1" checked><span>ON</span>
+                                <label class="form-check-label" for="radio1"></label>
+                            </div>
+                        </td>
+
+                        <td><img class="img-fluid" src="/views/assets/img/template/1/logo.png" style="width: 100px;"></td>
+
+                        <td><img class="img-fluid" src="/views/assets/img/template/1/icono.png" style="width: 25px;"></td>
+
+                        <td><img class="img-thumbnail" src="/views/assets/img/template/1/cover.jpg" style="width: 100px;"></td>
+
+                        <td>Sistema Ecommerce</td>
+
+                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores aliquam nemo, corrupti...</td>
+
+                        <td>
+                            <div class="btn-group">
+                                <a href="" class="btn bg-purple border-0 rounded-pill mr-2 btn-sm px-3">
+                                    <i class="fas fa-pencil-alt text-white"></i>
+                                </a>
+                                <a href="" class="btn btn-dark border-0 rounded-pill mr-2 btn-sm px-3">
+                                    <i class="fas fa-trash-alt text-white"></i>
+                                </a>
+                            </div>
+                        </td>
+                    </tr>
+
+                </tbody>
+
+            </table>
+
+        </div>
+
+    </div>
+
+</div>
